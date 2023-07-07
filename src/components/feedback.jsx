a3dfa39f0b332a87c80b813123661cc35ec1e46a
Heisenberg-8 <tanejasidak@gmail.comm>
2023-07-07 11:58:20 +0530

    msg

---
+++ src/components/feedback.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Feedback() {
+    return (
+        <div>
+            {/* Your feedback screen JSX */}
+            <h1>Feedback Screen</h1>
+        </div>
+    );
+}
+
+export default Feedback;
