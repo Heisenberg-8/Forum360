981f97fc71a551ea011c9508fe4e4278cd49db45
AarinK <aarin.kachroo@gmail.com>
2023-08-10 15:38:33 +0530

    RR

--- src/components/Analytics/Retention.jsx
+++ src/components/Analytics/Retention.jsx
@@ -1,33 +1,47 @@
-  import React from 'react';
-  import "./Sentiment.css"
-  import pin from '../assets/pin.svg';
+import React, { useState, useEffect } from 'react';
+import "./Sentiment.css"
+import pin from '../assets/pin.svg';
+import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend, PointElement, Filler } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-  import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    PointElement,
-    Filler,
-  } from 'chart.js';
-  import { Line } from 'react-chartjs-2';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  Filler,
+);
 
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    PointElement,
-    Filler,
-  );
+const RR = () => {
+  const labels = ['', '', '', '', ''];
+  const dataset1Data = [20, 40, 30, 35, 30];
+  const lastPointIndex = dataset1Data.length - 1;
 
+  const initialData = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: new Array(dataset1Data.length).fill(0),
+        borderColor: '#9747FF',
+        backgroundColor: 'rgba(151, 71, 255, 0.5)',
+        fill: {
+          target: 'origin',
+          above: 'rgba(151, 71, 255, 0.15)',
+        },
+        point: true,
+        pointRadius: dataset1Data.map((_, index) => (index === lastPointIndex ? 5 : 0)),
+        pointBorderColor: dataset1Data.map((_, index) => (index === lastPointIndex ? 'white' : '#9747FF')),
+        pointBackgroundColor: dataset1Data.map((_, index) => (index === lastPointIndex ? '#9747FF' : 'rgba(151, 71, 255, 0.5)')),
+        pointBorderWidth: dataset1Data.map((_, index) => (index === lastPointIndex ? 2 : 0)),
+      },
+    ],
+  };
 
-
+  const [data, setData] = useState(initialData);
 
   const options = {
     responsive: true,
@@ -48,7 +62,6 @@
           display: false,
         },
         color: 'white',
-      
       },
       y: {
         display: true,
@@ -61,59 +74,55 @@
     },
   };
 
+  useEffect(() => {
+    const animationDuration = 3000; // Adjust the animation duration as needed
+    let animationFrame;
+    let frame = 0;
 
+    const animateData = () => {
+      if (frame >= dataset1Data.length) {
+        cancelAnimationFrame(animationFrame);
+        return;
+      }
 
-  const labels = ['', '', '', '', ''];
-  const dataset1Data = [20, 40, 30, 35, 30];
-  const lastPointIndex = dataset1Data.length - 1;
+      const newData = {
+        ...data,
+        datasets: [
+          {
+            ...data.datasets[0],
+            data: dataset1Data.slice(0, frame + 1),
+          },
+        ],
+      };
+      setData(newData);
 
+      frame++;
+      animationFrame = requestAnimationFrame(animateData);
+    };
 
+    animationFrame = requestAnimationFrame(animateData);
 
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: dataset1Data,
-        borderColor: '#9747FF',
-        backgroundColor: 'rgba(151, 71, 255, 0.5)', 
-        fill: {
-          target: 'origin',
-          above: 'rgba(151, 71, 255, 0.15)',
+    return () => {
+      cancelAnimationFrame(animationFrame);
+    };
+  }, []);
 
-        },
-        point: true,
-        pointRadius: dataset1Data.map((_, index) => (index === lastPointIndex ? 5 : 0)), 
-        pointBorderColor: dataset1Data.map((_, index) => (index === lastPointIndex ? 'white' : '#9747FF')), 
-        pointBackgroundColor: dataset1Data.map((_, index) => (index === lastPointIndex ? '#9747FF' : 'rgba(151, 71, 255, 0.5)')), 
-        pointBorderWidth: dataset1Data.map((_, index) => (index === lastPointIndex ? 2 : 0)), 
-
-      },
-    ],
-  };
-
-
-
-
-
-
-
-  function RR() {
-    return (
-      <div className="bigContainer">
-      <div style={{ display: "flex" }}>
-            <text className="headingSmallCard"  style={{ marginLeft: "6%", marginTop: "3%",whiteSpace: 'nowrap'  }}>Retention Rates</text>
-            <text className="headingSmallCard"  style={{ marginLeft: "14%", marginTop: "3%" }}>&gt;90%</text>
-            <img src={pin} style={{ marginLeft: "5%", marginRight: "3%", marginTop: "1%" }} />
-          </div>
-          <text style={{ fontFamily: "albert", fontSize: "12px", color: "#b1afaf", marginLeft: "6%" }}>98 of 163 Registered</text>
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "100%", marginTop: "-20px" }}>
-    <div style={{ width: "90%", marginLeft: "0px", marginTop: "10px" }}>
-      <Line options={options} data={data} />
-    </div>
+  return (
+    <div className="bigContainer">
+    <div style={{ display: "flex" }}>
+          <text className="headingSmallCard"  style={{ marginLeft: "6%", marginTop: "3%",whiteSpace: 'nowrap'  }}>Retention Rates</text>
+          <text className="headingSmallCard"  style={{ marginLeft: "14%", marginTop: "3%" }}>&gt;90%</text>
+          <img src={pin} style={{ marginLeft: "5%", marginRight: "3%", marginTop: "1%" }} />
+        </div>
+        <text style={{ fontFamily: "albert", fontSize: "12px", color: "#b1afaf", marginLeft: "6%" }}>98 of 163 Registered</text>
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "100%", marginTop: "-20px" }}>
+  <div style={{ width: "90%", marginLeft: "0px", marginTop: "10px" }}>
+    <Line options={options} data={data} />
   </div>
+</div>
 
-      </div>
-    );
-  }
-  export default RR;
+    </div>
+  );
+};
+
+export default RR;
